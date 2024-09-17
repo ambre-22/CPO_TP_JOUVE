1fd0c776157619d50c820314fb0633a4ead2d067
Ambre Jouve <ambre.jouve@orange.frepfedu.fr>
2024-09-17 12:59:20 +0200

    Création d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,34 @@
+/*  Jouve Ambre FT2 Présentation de Netbeans 17/09/2024
+
+System.out.println("Bonjour");
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+/**
+ *
+ * @author ambre
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        // TODO code application logic here
+        System.out.println("Au revoir");
+        
+                           String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+prenom = sc.nextLine();
+        
+    }
+    
+
+    
+}
