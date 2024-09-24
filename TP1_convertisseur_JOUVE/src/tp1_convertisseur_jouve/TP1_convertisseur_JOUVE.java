c93a3f87ada8632193722704a791142db03a747b
Ambre Jouve <ambre.jouve@orange.frepfedu.fr>
2024-09-24 18:56:07 +0200

    Création d'un nouveau projet versionné

--- TP1_convertisseur_JOUVE/src/tp1_convertisseur_jouve/TP1_convertisseur_JOUVE.java
+++ TP1_convertisseur_JOUVE/src/tp1_convertisseur_jouve/TP1_convertisseur_JOUVE.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class TP1_convertisseur_JOUVE {
 
     /**
-     * @param args the command line arguments
+     * @param args
      */
     public static void main(String[] args) {
         // TODO code application logic here
@@ -31,32 +31,65 @@ public class TP1_convertisseur_JOUVE {
         System.out.println("La temperature correspondante en degres Kelvin est : " + temperatureKelvin);
   
     }
-     // Méthode pour convertir Kelvin vers Celsius
+    
+     // Convertir Celsius en Kelvin
+    public static double CelciusVersKelvin(double tCelcius) {
+        return tCelcius + 273.15;
+    }
+     // Convertir Kelvin en Celsius
     public static double KelvinVersCelcius(double tKelvin) {
         return tKelvin - 273.15;
     }
-
-    // Méthode pour convertir Fahrenheit vers Celsius
-    public static double FahrenheitVersCelcius(double tFahrenheit) {
-        return (tFahrenheit - 32) * 5 / 9;
+     // Convertir Fahrenheit en Celsius
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        return (tFarenheit - 32) * 5 / 9;
+    }
+    
+    // Convertir Celsius en Fahrenheit
+    public static double CelciusVersFarenheit(double tCelcius) {
+        return (tCelcius * 9 / 5) + 32;
+    }
+    
+    // Convertir Kelvin en Fahrenheit (via Celsius)
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tCelcius = KelvinVersCelcius(tKelvin);
+        return CelciusVersFarenheit(tCelcius);
     }
 
-    // Méthode pour convertir Celsius vers Fahrenheit
-    public static double CelciusVersFahrenheit(double tCelsius) {
-        return (tCelsius * 9 / 5) + 32;
-    }
-
-    // Méthode pour convertir Kelvin vers Fahrenheit
-    public static double KelvinVersFahrenheit(double tKelvin) {
-        // Conversion via Celsius
-        double tCelsius = KelvinVersCelcius(tKelvin);
-        return CelciusVersFahrenheit(tCelsius);
-    }
-
-    // Méthode pour convertir Fahrenheit vers Kelvin
-    public static double FahrenheitVersKelvin(double tFahrenheit) {
-        // Conversion via Celsius
-        double tCelsius = FahrenheitVersCelcius(tFahrenheit);
-        return CelciusVersKelvin(tCelsius);}
+    // Méthode pour convertir Fahrenheit en Kelvin (via Celsius)
+public static double FarenheitVersKelvin(double tFahrenheit) {
+    double tCelcius = FarenheitVersCelcius(tFahrenheit);
+    return CelciusVersKelvin(tCelcius);
 }
-     
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrez une température en Celsius : ");
+        double tCelcius = scanner.nextDouble();
+        
+        // Conversion et affichage
+        double tKelvin = CelciusVersKelvin(tCelcius);
+        System.out.println("En Kelvin, cette valeur vaut: " + tKelvin);
+
+        double tFarenheit = CelciusVersFarenheit(tCelcius);
+        System.out.println("En Farenhiet, cette valeur vaut : " + tFarenheit);
+
+        System.out.println("Entrez une température en Kelvin : ");
+        double tKelvinInput = scanner.nextDouble();
+        double tCelciusFromKelvin = KelvinVersCelcius(tKelvinInput);
+        System.out.println("En Celcius, cette valeur vaut : " + tCelciusFromKelvin);
+        
+        double tFarenheitFromKelvin = KelvinVersFarenheit(tKelvinInput);
+        System.out.println("En Farenheit, cette valeur vaut : " + tFarenheitFromKelvin);
+
+        System.out.println("Entrez une température en Fahrenheit : ");
+        double tFarenheitInput = scanner.nextDouble();
+        double tCelciusFromFarenheit = FarenheitVersCelcius(tFarenheitInput);
+        System.out.println("En Celcius, cette valeur vaut : " + tCelciusFromFarenheit);
+
+        double tKelvinFromFarenheit = FarenheitVersKelvin(tFarenheitInput);
+        System.out.println("En Kelvin, cette valeur  : " + tKelvinFromFarenheit); }
+
+}
+
+    
