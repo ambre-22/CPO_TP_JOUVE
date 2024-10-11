0adebd240e5d2eb123f1ea4cb2b0287bc101d225
Ambre Jouve <ambre.jouve@orange.frepfedu.fr>
2024-10-11 09:00:15 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_JOUVE/src/tp2_bieres_jouve/BouteilleBiere.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_jouve;
+
+/**
+ *
+ * @author ambre
+ */
+public class BouteilleBiere {
+    String nom; // Le nom de la bière
+    float degreAlcool; // Son degré d’alcool
+    String brasserie; // Le nom de la brasserie
+    boolean ouverte; // Indique si la bouteille est ouverte
+
+    BouteilleBiere(String leffe, double d, String abbaye_de_Leffe) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    BouteilleBiere() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+
+
+}
+public BouteilleBiere(String unNom, float unDegre, String
+uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+
+public boolean decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            System.out.println(nom + " est maintenant ouverte.");
+            return true; // Succès
+        } else {
+            System.out.println("Erreur : bière déjà ouverte.");
+            return false; // Échec
+        }
+    }}
